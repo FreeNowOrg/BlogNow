43cff0780ecdf3904033cb781ec55ea57521a2b1
机智的小鱼君 <dragon-fish@qq.com>
2021-10-20 23:24:59 +0800

    chore: minor fix

--- src/utils/FileUploader.ts
+++ src/utils/FileUploader.ts
@@ -1,21 +1,41 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 import { nanoid } from 'nanoid'
 
 export class FileUploader {
-  private repo: string
-  request: AxiosInstance
+  private repo!: string
+  private token!: string
+  request!: AxiosInstance
 
+  /**
+   * @param token e.g. `ghp_ABCabc123`
+   * @param repo e.g. `username/reponame`
+   */
   constructor(token: string, repo: string) {
-    this.repo = repo
-    this.request = axios.create({
+    this.setToken(token)
+    this.setRepo(repo)
+  }
+
+  private getRequest() {
+    return axios.create({
       baseURL: 'https://api.github.com',
       headers: {
-        authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${this.token}`,
       },
     })
   }
 
-  getFilePath(name: string, ext: string) {
+  setToken(token: string) {
+    this.token = token
+    this.request = this.getRequest()
+    return this
+  }
+
+  setRepo(repo: string) {
+    this.repo = repo
+    return this
+  }
+
+  private generateFilePath(name: string, ext: string) {
     const now = new Date()
     const yyyy = now.getFullYear()
     const mm = `0${now.getMonth() + 1}`.slice(-2)
@@ -42,59 +62,52 @@ export class FileUploader {
     })
   }
 
-  async getFileList(): Promise<
-    {
-      path: string
-      mode: string
-      type: string
-      sha: string
-      size: number
-      url: string
-    }[]
-  > {
+  async getFileList(): Promise<FilePayload[]> {
     const { data }: any = await this.request.get(
       `/repos/${this.repo}/git/trees/HEAD:uploads`,
       {
         params: {
           recursive: 1,
+          _random: Math.random(),
         },
       }
     )
-    return data.tree
+    const list = (data.tree as FilePayload[])
+      .filter(({ type }) => type === 'blob')
+      .map((i) => {
+        i.html_url = `https://github.com/${this.repo}/tree/HEAD/uploads/${i.path}`
+        i.name = i.path.split('/').pop() as string
+        return i
+      })
+    console.log(list)
+    return list
   }
 
-  getFileUrl(payload: { path: string }) {
+  /**
+   * @desc Get file raw url and proxy urls
+   */
+  getFileUrl(payload: FilePayload) {
     const file = `${this.repo}/HEAD/uploads/${payload.path}`
     return {
       raw: `https://raw.githubusercontent.com/${file}`,
-      jsdelivr: `https://cdn.jsdelivr.net/gh/${file}`,
       fastgit: `https://raw.fastgit.org/${file}`,
-      zwc365: `https://pd.zwc365.com/https://raw.githubusercontent.com/${file}`,
       ghproxy: `https://ghproxy.com/https://raw.githubusercontent.com/${file}`,
+      jsdelivr: `https://cdn.jsdelivr.net/gh/${this.repo}/uploads/${payload.path}`,
+      zwc365: `https://pd.zwc365.com/https://raw.githubusercontent.com/${file}`,
     }
   }
 
-  async upload(file: File): Promise<
-    AxiosResponse<{
-      name: string
-      path: string
-      sha: string
-      size: number
-      url: string
-      html_url: string
-      download_url: string
-    }>
-  > {
+  async upload(file: File): Promise<AxiosResponse<{ content: FilePayload }>> {
     const name = nanoid(8)
     const { base64, ext } = await this.getFileBase64(file)
-    const path = this.getFilePath(name, ext)
+    const path = this.generateFilePath(name, ext)
     return this.request.put(`/repos/${this.repo}/contents/uploads/${path}`, {
       message: `upload file "${file.name}"`,
       content: base64,
     })
   }
 
-  async delete(payload: { name: string; path: string; sha: string }): Promise<
+  async delete(payload: FilePayload): Promise<
     AxiosResponse<{
       commit: {
         sha: string
@@ -118,4 +131,16 @@ export class FileUploader {
       }
     )
   }
+}
+
+export interface FilePayload {
+  name: string
+  path: string
+  sha: string
+  size: number
+  url: string
+  html_url: string
+  git_url: string
+  download_url: string
+  type: 'tree' | 'blob'
 }
